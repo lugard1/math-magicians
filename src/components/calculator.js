c5b4318616158e69888509d9f27b7dcd9e8dcab0
Lucas David Erkana <lderkana21x@gmail.com>
2023-01-14 09:41:50 +0200

    Fix UI

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -3,38 +3,44 @@ import React from 'react';
 class Calculator extends React.PureComponent {
   render() {
     return (
-      <div className="calculator">
-        <div className="calculator-display">0</div>
-        <div className="calculator-keypad">
-          <div className="input-keys">
-            <div className="function-keys">
-              <button type="button" className="calculator-key key-clear">AC</button>
-              <button type="button" className="calculator-key key-sign">+/-</button>
-              <button type="button" className="calculator-key key-percent">%</button>
-            </div>
-            <div className="digit-keys">
-              <button type="button" className="calculator-key key-0">0</button>
-              <button type="button" className="calculator-key key-dot">&#x2022;</button>
-              <button type="button" className="calculator-key key-1">1</button>
-              <button type="button" className="calculator-key key-2">2</button>
-              <button type="button" className="calculator-key key-3">3</button>
-              <button type="button" className="calculator-key key-4">4</button>
-              <button type="button" className="calculator-key key-5">5</button>
-              <button type="button" className="calculator-key key-6">6</button>
-              <button type="button" className="calculator-key key-7">7</button>
-              <button type="button" className="calculator-key key-8">8</button>
-              <button type="button" className="calculator-key key-9">9</button>
-            </div>
-          </div>
-          <div className="operator-keys">
-            <button type="button" className="calculator-key key-divide">&divide;</button>
-            <button type="button" className="calculator-key key-multiply">x</button>
-            <button type="button" className="calculator-key key-subtract">-</button>
-            <button type="button" className="calculator-key key-add">+</button>
-            <button type="button" className="calculator-key key-equals">=</button>
-          </div>
+      <div className="calculator-keypad">
+        <div className="input-keys">
+          <button type="button">AC</button>
+          <button type="button">+/-</button>
+          <button type="button">%</button>
+          <button type="button" className="operators">
+            &divide;
+          </button>
+          <button type="button">7</button>
+          <button type="button">8</button>
+          <button type="button">9</button>
+
+          <button type="button" className="operators">
+            x
+          </button>
+          <button type="button">4</button>
+          <button type="button">5</button>
+          <button type="button">6</button>
+          <button type="button" className="operators">
+            -
+          </button>
+          <button type="button">1</button>
+          <button type="button">2</button>
+          <button type="button">3</button>
+          <button type="button" className="operators">
+            +
+          </button>
+          <button type="button" className="zerodigit">
+            0
+          </button>
+          <button type="button">.</button>
+          <button type="button" className="operators">
+            =
+          </button>
+
         </div>
       </div>
+
     );
   }
 }
